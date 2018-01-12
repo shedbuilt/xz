8c3a134877ae60a9c29ab341793aa8435bb9a7cb
Auston Stewart <auston@shedbuilt.net>
2018-01-12 12:24:30 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-./configure --prefix=/usr    \
-            --disable-static \
-            --docdir=/usr/share/doc/xz-5.2.3
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/bin
-mv -v   ${SHED_FAKEROOT}/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} ${SHED_FAKEROOT}/bin
-mkdir -v ${SHED_FAKEROOT}/lib
-mv -v ${SHED_FAKEROOT}/usr/lib/liblzma.so.* ${SHED_FAKEROOT}/lib
-ln -svf ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/liblzma.so) ${SHED_FAKEROOT}/usr/lib/liblzma.so
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        ;;
+    *)
+        ./configure --prefix=/usr    \
+                    --disable-static \
+                    --docdir=/usr/share/doc/xz-5.2.3 || return 1
+        ;;
+esac
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+
+# Rearrange binaries
+if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
+    mkdir -v "${SHED_FAKEROOT}/bin"
+    mv -v ${SHED_FAKEROOT}/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} "${SHED_FAKEROOT}/bin"
+    mkdir -v "${SHED_FAKEROOT}/lib"
+    mv -v ${SHED_FAKEROOT}/usr/lib/liblzma.so.* "${SHED_FAKEROOT}/lib"
+    ln -svf ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/liblzma.so) "${SHED_FAKEROOT}/usr/lib/liblzma.so"
+fi
