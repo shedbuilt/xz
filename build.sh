81cac5e0e40d3149219ec6a5eed1fcf8a27ddace
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:29:52 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
         ./configure --prefix=/tools || return 1
         ;;
@@ -9,14 +9,14 @@ case "$SHED_BUILDMODE" in
                     --docdir=/usr/share/doc/xz-5.2.3 || return 1
         ;;
 esac
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+make -j $SHED_NUM_JOBS || return 1
+make DESTDIR="$SHED_FAKE_ROOT" install || return 1
 
 # Rearrange binaries
-if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
-    mkdir -v "${SHED_FAKEROOT}/bin"
-    mv -v ${SHED_FAKEROOT}/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} "${SHED_FAKEROOT}/bin"
-    mkdir -v "${SHED_FAKEROOT}/lib"
-    mv -v ${SHED_FAKEROOT}/usr/lib/liblzma.so.* "${SHED_FAKEROOT}/lib"
-    ln -svf ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/liblzma.so) "${SHED_FAKEROOT}/usr/lib/liblzma.so"
+if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
+    mkdir -v "${SHED_FAKE_ROOT}/bin"
+    mv -v ${SHED_FAKE_ROOT}/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} "${SHED_FAKE_ROOT}/bin"
+    mkdir -v "${SHED_FAKE_ROOT}/lib"
+    mv -v ${SHED_FAKE_ROOT}/usr/lib/liblzma.so.* "${SHED_FAKE_ROOT}/lib"
+    ln -svf ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/liblzma.so) "${SHED_FAKE_ROOT}/usr/lib/liblzma.so"
 fi
--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     # Remove temporary symlinks created earlier in the bootstrap
     rm -v /usr/lib/liblzma.so.*
 fi
