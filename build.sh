c4fc03fff7f3e53cdb85b5a873dd2db09ef30fd3
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:05:07 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools || return 1
-        ;;
-    *)
-        ./configure --prefix=/usr    \
-                    --disable-static \
-                    --docdir=/usr/share/doc/xz-5.2.3 || return 1
-        ;;
-esac
-make -j $SHED_NUM_JOBS || return 1
-make DESTDIR="$SHED_FAKE_ROOT" install || return 1
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_PREFIX='/usr'
+SHED_PKG_LOCAL_STATIC_OPTION='--disable-static'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+    SHED_PKG_LOCAL_STATIC_OPTION=''
+fi
+SHED_PKG_LOCAL_DOCDIR=${SHED_PKG_LOCAL_PREFIX}/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}
 
-# Rearrange binaries
-if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
-    mkdir -v "${SHED_FAKE_ROOT}/bin"
-    mv -v ${SHED_FAKE_ROOT}/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} "${SHED_FAKE_ROOT}/bin"
-    mkdir -v "${SHED_FAKE_ROOT}/lib"
-    mv -v ${SHED_FAKE_ROOT}/usr/lib/liblzma.so.* "${SHED_FAKE_ROOT}/lib"
-    ln -svf ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/liblzma.so) "${SHED_FAKE_ROOT}/usr/lib/liblzma.so"
+# Configure
+./configure --prefix=${SHED_PKG_LOCAL_PREFIX} \
+            --docdir=${SHED_PKG_LOCAL_DOCDIR} \
+            ${SHED_PKG_LOCAL_STATIC_OPTION} &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+
+# Rearrange
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    mkdir -v "${SHED_FAKE_ROOT}/bin" &&
+    mv -v "${SHED_FAKE_ROOT}"/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} "${SHED_FAKE_ROOT}/bin" &&
+    mkdir -v "${SHED_FAKE_ROOT}/lib" &&
+    mv -v "${SHED_FAKE_ROOT}"/usr/lib/liblzma.so.* "${SHED_FAKE_ROOT}/lib" &&
+    ln -svf ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/liblzma.so") "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/lib/liblzma.so" || exit 1
+fi
+
+# Manage Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
 fi
--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
     # Remove temporary symlinks created earlier in the bootstrap
     rm -v /usr/lib/liblzma.so.*
 fi
